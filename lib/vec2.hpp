bd33d6505d5606101568aebe42901dca98457b63
Gaven Behrends <gzbehrends@gmail.com>
2024-12-16 16:18:56 -0600

    vec2 update

--- lib/vec2.hpp
+++ lib/vec2.hpp
@@ -1,6 +1,6 @@
 struct Vec2 {
-    int x;
-    int y;
+    long x;
+    long y;
 };
 
 inline Vec2 operator+(Vec2 const& lhs, Vec2 const& rhs) {
@@ -11,6 +11,23 @@ inline Vec2 operator-(Vec2 const& lhs, Vec2 const& rhs) {
     return Vec2{ lhs.x - rhs.x, lhs.y - rhs.y };
 }
 
+inline Vec2 operator*(Vec2 const& lhs, Vec2 const& rhs) {
+    return Vec2{ lhs.x * rhs.x, lhs.y * rhs.y };
+}
+
+inline Vec2 operator/(Vec2 const& lhs, Vec2 const& rhs) {
+    return Vec2{ lhs.x / rhs.x, lhs.y / rhs.y};
+}
+
 inline bool operator==(Vec2 const& lhs, Vec2 const& rhs) {
     return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+inline bool operator<(Vec2 const& lhs, Vec2 const& rhs) {
+    if (lhs.x < rhs.x) {
+        return true;
+    } else if (lhs.x == rhs.x && lhs.y < rhs.y) {
+        return true;
+    }
+    return false;
 }
